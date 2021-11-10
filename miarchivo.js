a5861f112e33e29a49278af94f59c1ce26a24bbb
Melisa Varela <mabrilvarela@gmail.com>
2021-11-10 11:22:14 -0300

    Desafio 2

---
+++ miarchivo.js
@@ -0,0 +1,11 @@
+let numero = parseInt(prompt("Ingrese numero"));
+
+if (numero >= 10 &&  numero <= 50) {
+
+    alert("Tu numero es positivo");
+
+} else {
+
+    alert("Tu numero es negativo");
+
+}
